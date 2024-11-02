acbdc8b4a11dd6e94f7f72540b3dbefd48d3312f
basia1111 <basia.zygilewicz@gmail.com>
2024-11-02 11:50:16 +0100

    Update server configuration to support dev and production environments

--- server/server.js
+++ server/server.js
@@ -5,11 +5,17 @@ const mongoose = require('mongoose')
 const tasksRouter = require('./routes/tasks')
 const usersRouter = require('./routes/users')
 require('dotenv').config()
+const mode = process.env.NODE_ENV
 
+const allowedOrigins = {
+    production: ['https://rockettasks.vercel.app'],
+    development:['http://localhost:5173']
+}
 app.use(express.json())
 app.use(cors({
-    origin: ["https://rockettasks.vercel.app"]
-}))
+    origin: allowedOrigins[mode]
+}));
+
 app.use('/api/tasks', tasksRouter )
 app.use('/api/users', usersRouter )
 
@@ -21,4 +27,9 @@ mongoose.connect(process.env.DATABASE_URL)
     .then(() => console.log('connected to database'))
     .catch((err) => console.log(err.message))
 
+if(mode == 'development'){
+    app.listen(3000,()=> {
+        console.log('server is running on port 3000')
+    })
+}
 module.exports = app
